6926c812d5a3777c64fa02ea5b31406fb08493b3
g-almeid-a <ga.almeida1923@gmail.com>
2025-07-02 12:43:41 -0300

    alterando a imagem e testando deploy do vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,7 @@ export default function Home() {
       <div className="h-20" />
 
       <section className="flex flex-col items-center justify-center text-center py-20 px-6">
-        <img src="/logoMelhorQualidade.png" alt="Logo Gravelo" className="w-40 h-auto mb-6" />
-        <h1 className="text-4xl font-bold tracking-wide">GRAVELO</h1>
-        <p className="mt-4 text-lg italic text-gray-700">Estilo não se veste – se vive!</p>
+        <img src="/logoMelhorQualidade.png" alt="Logo Gravelo" className="fixed bottom-0 rigth-0 w-40 h-auto mb-6" />
       </section>
     </main>
   );
