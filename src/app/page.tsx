8d74342a7a49771ad794f6e25f5923347df59852
g-almeid-a <ga.almeida1923@gmail.com>
2025-07-02 12:49:10 -0300

    imagem alterada novamente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="h-20" />
 
       <section className="flex flex-col items-center justify-center text-center py-20 px-6">
-        <img src="/logoMelhorQualidade.png" alt="Logo Gravelo" className="fixed bottom-0 rigth-0 w-40 h-auto mb-6" />
+        <img src="/LogoMelhorQualidade.png" alt="Logo Gravelo" className="fixed bottom-0 rigth-0 w-40 h-auto mb-6" />
       </section>
     </main>
   );
